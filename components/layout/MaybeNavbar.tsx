8f5e61e198e89a1bb15972adfdc5e70e6dfb8c57
SaiyedAbdullah-127 <tafs4918@gmail.com>
2026-06-15 19:06:42 +0500

    one fix

--- components/layout/MaybeNavbar.tsx
+++ components/layout/MaybeNavbar.tsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
  
- const MaybeNavbar: React.FC = () => {
+const MaybeNavbar: React.FC = () => {
   return <Navbar />;
+};
 
 export default MaybeNavbar;
